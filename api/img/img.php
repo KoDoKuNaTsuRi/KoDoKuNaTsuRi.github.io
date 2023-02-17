fb40fe787b8dd5432c3a24d1520346a57b7d6cbb
KoDoKuNaTsuRi <3046162104@qq.com>
2023-02-17 13:34:50 +0800

    Site updated: 2023-02-17 13:34:50

--- api/img/img.php
+++ api/img/img.php
@@ -1,6 +1,6 @@
 <?php
 $seed = time();
 $num = rand(1,3);
-$picpath = "https://kodokunatsuri.github.io/img/ (".$num.").jpg";
+$picpath = "https://kodokunatsuri.github.io/api/img/ (".$num.").jpg";
 die(header("Location: $picpath"));
 ?>
